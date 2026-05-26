43159b6c7be83b38e6bdb9da86a08c1c1d2c9240
mushipi <148541634+mushipi@users.noreply.github.com>
2026-05-26 22:49:17 +0900

    security: replace hardcoded Tailscale IP with variable

--- pi-scripts/sync_to_n97.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rsync -az --remove-source-files \
-  -e "ssh -i /home/mushipi/.ssh/id_ed25519_n97" \
-  /home/mushipi/recordings/ \
-  mushipi@100.102.9.77:/mnt/hamcam/BirdProject/raw_ingest/mushipi-bird01/
